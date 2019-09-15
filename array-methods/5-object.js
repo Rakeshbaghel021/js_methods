c0c957d425573b117df14e5ccd04d23c54fb2c99
Rakeshbaghel021 <rkrkumar021@gmail.com>
2019-09-15 22:34:18 +0530

    update js

--- array-methods/5-object.js
+++ array-methods/5-object.js
@@ -10,6 +10,7 @@ var out=Object.keys.(list);
 
 
 // 2. Write a JavaScript program to delete the rollno property from the following object. Also print the object before or after deleting the property.
+
 var student = { 
   name : "David Rayy", 
   sclass : "VI", 
